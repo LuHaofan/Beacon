301aa77b6a17ad6f9e3753345f4a51c9df039903
Lu Haofan <haofan.17@intl.zju.edu.cn>
2020-07-25 12:05:42 -0500

    update decode

--- Backdoor/decode.m
+++ Backdoor/decode.m
@@ -44,20 +44,39 @@ function re = decode(data, code)
     signal = cat(2, signal ,s2);
     corr = xcorr(data, signal);
     [M I] = max(corr);
-    sig_length = 0.07*48000+1;
-    sample_length = 512;
-    sample0_s = data(I+sample_length: I+2*sample_length);
-    sample0_e = data(I+sig_length-2*sample_length:I+sig_length-sample_length);
-    sample1_s = data(I+sig_length+sample_length:I+sig_length+2*sample_length);
-    sample1_e = data(I+2*sig_length-2*sample_length:I+2*sig_length-sample_length);
-    sample2_s = data(I+2*sig_length+sample_length:I+2*sig_length+2*sample_length);
-    sample2_e = data(I+3*sig_length-2*sample_length:I+3*sig_length-sample_length);
-    fft0_s = fft(sample0_s);
-    fft0_e = fft(sample0_e);
-    fft1_s = fft(sample1_s);
-    fft1_e = fft(sample1_e);
-    fft2_s = fft(sample2_s);
-    fft2_e = fft(sample2_e);
-    
+    if M/mean(corr(I-length(signal):I+length(signal))) > 10
+        sig_length = 0.07*48000+1;
+        sample_length = 512;
+        sample0_s = data(I+sample_length: I+2*sample_length);
+        sample0_e = data(I+sig_length-2*sample_length:I+sig_length-sample_length);
+        sample1_s = data(I+sig_length+sample_length:I+sig_length+2*sample_length);
+        sample1_e = data(I+2*sig_length-2*sample_length:I+2*sig_length-sample_length);
+        sample2_s = data(I+2*sig_length+sample_length:I+2*sig_length+2*sample_length);
+        sample2_e = data(I+3*sig_length-2*sample_length:I+3*sig_length-sample_length);
+        fft0_s = max(abs(fft(sample0_s)));
+        fft0_e = max(abs(fft(sample0_e)));
+        fft1_s = max(abs(fft(sample1_s)));
+        fft1_e = max(abs(fft(sample1_e)));
+        fft2_s = max(abs(fft(sample2_s)));
+        fft2_e = max(abs(fft(sample2_e)));
+
+        if (fft0_s > fft0_e && fft1_s > fft1_e && fft2_s > fft2_e && code == 0)
+            re = true;
+        elseif (fft0_s < fft0_e && fft1_s > fft1_e && fft2_s > fft2_e && code == 1)
+            re = true;
+        elseif (fft0_s > fft0_e && fft1_s < fft1_e && fft2_s > fft2_e && code == 2)
+            re = true;
+        elseif (fft0_s < fft0_e && fft1_s < fft1_e && fft2_s > fft2_e && code == 3)
+            re = true;
+        elseif (fft0_s > fft0_e && fft1_s > fft1_e && fft2_s < fft2_e && code == 4)
+            re = true;
+        elseif (fft0_s < fft0_e && fft1_s > fft1_e && fft2_s < fft2_e && code == 5)
+            re = true;
+        elseif (fft0_s > fft0_e && fft1_s < fft1_e && fft2_s < fft2_e && code == 6)
+            re = true;
+        elseif (fft0_s < fft0_e && fft1_s < fft1_e && fft2_s < fft2_e && code == 7)
+            re = true;
+        end
+    end
 end
 
